e2971c1d4a45c3913897702c2063d3cb0c1fad51
Margarita Ignatieva <margaret.vegan@gmail.com>
2016-07-06 18:03:37 +0300

    placeholder

--- js/comment.js
+++ js/comment.js
@@ -1,10 +1,24 @@
 
 var dummy = 'Sed quis diam egestas, egestas mauris in, dapibus eros. Duis nisi nulla, accumsan eu libero sit amet, faucibus ornare nisi. Phasellus cursus dolor ante, at placerat est tincidunt vel. In ullamcorper pulvinar est id congue. Pellentesque scelerisque ante vel justo varius, non aliquet est eleifend. Aliquam erat volutpat. Curabitur blandit, lorem eget tincidunt scelerisque, mauris felis pellentesque dolor, et adipiscing nisi ipsum eget est.';
 
-function AppViewModel() {
-  this.comment = ko.observable(dummy);
+function Comment(dummy) {
+  this.comment = ko.observdummy;
+
+  this.addComment = function addComment(comm) {
+    this.comment = comm;
+  }
 }
+var viewModel = Comment(dummy)
+ko.applyBindings(viewModel);
 
-ko.applyBindings(new AppViewModel());
+var commentInput = document.getElementById('comment-input');
+commentInput.addEventListener('keyup', getComment);
 
-document.getElementById('comment-input').placeholder = "Type name here..";
+function getComment(e) {
+	var msg;
+  if (e.keyCode === 13) {
+     msg = this.value;
+     console.log(msg);
+     viewModel.addComment(msg);
+  }
+}
