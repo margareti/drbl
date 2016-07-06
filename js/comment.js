c79e752aa2185839b17cb956c566bf39a5f952dc
Margarita Ignatieva <margaret.vegan@gmail.com>
2016-07-06 18:27:18 +0300

    message added, but placeholder needs to be changed

--- js/comment.js
+++ js/comment.js
@@ -22,3 +22,15 @@ function getComment(e) {
      viewModel.addComment(msg);
   }
 }
+
+ko.bindingHandlers.yourBindingName = {
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        // This will be called when the binding is first applied to an element
+        // Set up any initial state, event handlers, etc. here
+    },
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        // This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+    }
+};
