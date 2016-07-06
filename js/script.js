5ee3c9ff14830a3180dc3a72afbd5ffca9ed1d67
Margarita Ignatyeva <margaret.vegan@gmail.com>
2016-07-06 08:43:39 +0300

    js and styling, to add timer above contorols

---
+++ js/script.js
@@ -0,0 +1,52 @@
+(function() {
+
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+  var player;
+  function onYouTubeIframeAPIReady(id) {
+    player = new YT.Player('player', {
+      height: '412',
+      width: '806',
+      videoId: id,
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+  
+
+  var videoHandler = document.getElementById('videoid');
+  var videoWrap = document.querySelector('.video-wrap');
+
+  videoHandler.addEventListener('click', loadVid);
+
+  function loadVid() {
+    var video = document.getElementById('vid').value;
+    onYouTubeIframeAPIReady(video);
+  }
+  
+
+})();
+      
